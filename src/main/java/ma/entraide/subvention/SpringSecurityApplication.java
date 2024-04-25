1d960b387b859d1c26ca77f34cfb52af5eb3cdb2
ismail <ismailbouras1968@gmail.com>
2024-04-25 10:49:21 +0200

    core test

--- src/main/java/ma/entraide/subvention/SpringSecurityApplication.java
+++ src/main/java/ma/entraide/subvention/SpringSecurityApplication.java
@@ -15,6 +15,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -36,6 +38,18 @@ public class SpringSecurityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				//registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
 
 	@Bean
 	public CommandLineRunner runner(DeleguationService deleguationService) {
